6edaf750630911b88ad9d6b7422e1e05c0d79a46
Dmitriy <56391907+dimalenser@users.noreply.github.com>
2020-03-05 10:45:09 +0200

    pr9

---
+++ pr9/pr9/Class1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace pr9
+{
+    class Fraction
+    {
+        private double digit;
+        public Fraction()
+        {
+            digit = 0;
+        }
+
+        public Fraction(double d)
+        {
+            digit = d;
+        }
+
+        public void toString()
+        {
+            Console.WriteLine($"Digit in string - {digit}");
+        }
+        public double get_digit()
+        {
+            return digit;
+        }
+
+        public void set_digit(double d)
+        {
+            digit = d;
+        }
+        public static Fraction operator +(Fraction obj1, Fraction obj2)
+        {
+            Fraction frac = new Fraction();
+            frac.set_digit(obj1.get_digit() + obj2.get_digit());
+            return frac;
+        }
+
+        public static Fraction operator -(Fraction obj1, Fraction obj2)
+        {
+            Fraction frac = new Fraction();
+            frac.set_digit(obj1.get_digit() - obj2.get_digit());
+            return frac;
+        }
+
+        public static Fraction operator *(Fraction obj1, Fraction obj2)
+        {
+            Fraction frac = new Fraction();
+            frac.set_digit(obj1.get_digit() * obj2.get_digit());
+            return frac;
+        }
+
+        public static Fraction operator /(Fraction obj1, Fraction obj2)
+        {
+            Fraction frac = new Fraction();
+            frac.set_digit(obj1.get_digit() / obj2.get_digit());
+            return frac;
+        }
+
+    }
+
+}
---
+++ pr9/pr9/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace pr9
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Fraction a = new Fraction(10);
+            Fraction b = new Fraction(2);
+            Fraction d = a-b;
+            Console.WriteLine($"a = {a.get_digit()}");
+            Console.WriteLine($"b = {b.get_digit()}");
+            Console.WriteLine($"d = a -b = {d.get_digit()}");
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
