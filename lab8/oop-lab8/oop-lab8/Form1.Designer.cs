5a6a5eea3773e38fea73bc554954c75abe14a228
Dmitriy <56391907+dimalenser@users.noreply.github.com>
2020-03-23 16:52:09 +0200

    add pr8

---
+++ lab8/oop-lab8/oop-lab8/Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace oop_lab8
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(151, 12);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.Color = System.Drawing.Color.Red;
+            series1.Legend = "Legend1";
+            series1.Name = "Series2";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(164, 47);
+            this.chart1.TabIndex = 2;
+            this.chart1.Text = "chart1";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::oop_lab8.Properties.Resources.rivnist1;
+            this.pictureBox2.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox2.InitialImage")));
+            this.pictureBox2.Location = new System.Drawing.Point(-8, -3);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(153, 120);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(1167, 450);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.pictureBox2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+    }
+}
+
---
+++ lab8/oop-lab8/oop-lab8/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace oop_lab8
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            one();
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            one();
+        }
+
+        void one()
+        {
+            Graphics g = this.CreateGraphics(); //задаємо область малювання
+            g.Clear(Color.White);   //очищуємо область (заливаємо білим кольором)
+            Pen a = new Pen(Color.Blue, 1); //ручка для малювання осей
+            Pen b = new Pen(Color.Green, 2);//ручка для малювання графіка
+            Font drawFont = new Font("Arial", 12);  //шрифт яким будемо писати осі 
+            Font signatureFont = new Font("Arial", 7);  //шрифт яким будемо підписувати ділення осі
+            SolidBrush drawBrush = new SolidBrush(Color.Blue);  //колір цього шрифта
+            StringFormat drawFormat = new StringFormat();   //формат букв (для підпису)
+            drawFormat.FormatFlags = StringFormatFlags.DirectionRightToLeft;    //направлення тексту осі
+            int sizeWidth = Form1.ActiveForm.Width; //ширина вікна програми
+            int sizeHeight = Form1.ActiveForm.Height;   //висота вікна програми
+            //х, у - координати центру (очки 0)
+            Point center = new Point(((int)(sizeWidth/2)-8), (int)((sizeHeight / 2) - 19));
+            
+            ///додаємо код для малювання лсів та виведення підписів до них
+
+            g.DrawLine(a, 10, center.Y, center.X, center.Y);                //ось Х-
+            g.DrawLine(a, center.X, center.Y, 2 * center.X - 10, center.Y); //ось Х+
+            g.DrawLine(a, center.X, 10, center.X, center.Y);                //ось У+
+            g.DrawLine(a, center.X, center.Y, center.X, 2 * center.Y -10);  //ось У-
+            g.DrawString("X", drawFont, drawBrush, new PointF(2 * center.X - 5, center.Y + 10), drawFormat); //підписали ч
+            g.DrawString("Y", drawFont, drawBrush, new PointF(center.X + 30, 5), drawFormat);   //підписали у
+
+
+            g.DrawLine(a, center.X, 10, center.X + 5, 20);  //стрілки: у+
+            g.DrawLine(a, center.X, 10, center.X - 5, 20);  //у-
+            //аналогічно намалювати стрілки х+ х-
+
+
+            int stepForAxes = 25;   //відстань між діленням на осях
+            int lenghtShtrih = 3;   //половина довжини штришка ділення
+            int maxValueForAxesX = 16;  //максимальне знчення по осі Х
+            int maxValueForAxesY = 25;  //по осі У
+
+            float oneDelenieX = (float)maxValueForAxesX / ((float)center.X / (float)stepForAxes);
+            //то, чим підписувати ділення Х
+            float oneDelenieY = (float)maxValueForAxesY / ((float)center.Y / (float)stepForAxes);
+            //то, чим підписувати ділення У
+            for (int i = center.X, j = center.X, k = 1; i < 2 * center.X - 30; j -= stepForAxes, i+= stepForAxes, k++)
+            {
+                g.DrawLine(a, i, center.Y - lenghtShtrih, i, center.Y + lenghtShtrih);  //малюємо штрихи по осі Х вправо від центра
+                g.DrawLine(a, j, center.Y - lenghtShtrih, j, center.Y + lenghtShtrih);  //малюємо штрихи по осі Х вліво від центра
+
+                if(i < 2 * center.X - 55)
+                {
+                    g.DrawString((k * oneDelenieX).ToString("0.0"), signatureFont, drawBrush,
+                            new PointF(i + stepForAxes + 9, center.Y + 6), drawFormat); //підписуємо ділення +
+                    g.DrawString(((k * oneDelenieX).ToString("0.0").ToString()+ "-"), 
+                        signatureFont, drawBrush, new PointF(j - stepForAxes + 9, center.Y + 6), drawFormat);
+                }
+            }
+            //малюємо штрихи по осі У
+            //підписуємо ділення по осі У
+            //розраховуємо значення функції на заданому інтеревалі і побудуємо цим значенням графік
+            int numOfPoint = 100;   //кількість точок для розрахування функції
+
+            float[] first = new float[numOfPoint];  //точки для розрахування
+
+            for(int i = 0; i < numOfPoint; i++)
+            {
+                first[i] = (float)maxValueForAxesX / (float)numOfPoint * (i + 1) - 6;   //інтервал від -6 до 10
+            }
+
+            float[] second = new float[numOfPoint]; 
+            for(int i = 0; i< numOfPoint; i++)
+            {
+                second[i] = (float)(Math.Pow(Math.E, first[i] /2)*Math.Sin(2 *first[i]));
+            }
+
+            Point[] pointOne = new Point[numOfPoint];
+
+            float tempX = 1 / oneDelenieX * stepForAxes;    //розраховуємо нові координати
+            float tempY = 1 / oneDelenieY * stepForAxes;
+
+            for(int i = 0; i< numOfPoint; i++)
+            {
+                pointOne[i].X = center.X + (int)(first[i] * tempX); //перехід до нових координатах
+                pointOne[i].Y = center.Y + (int)(second[i] * tempY);
+            }
+
+            g.DrawLines(b, pointOne);   //малюємо графік (ломана лінія)
+            g.DrawCurve(b, pointOne);   //малюємо сплайном
+        }
+    }
+}
